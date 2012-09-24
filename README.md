22328cc5ec5c7229e190e2221c654ab61bfa7cfa
Serge Borbit <serge.borbit@gmail.com>
2012-09-24 10:33:39 +0300

    Started main implementation. Methods "add" and "rank". Tests. Readme.

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ Leaderboard
 -----------
 
 Leaderboards backed by [Redis](http://redis.io) in Node.js
+
+API
+---
+
+#Constructor
+
+    new Leaderboard('name', [options])
+
+Creates a new leaderboard or attaches to an existing leaderboard.
+
+**Options**
+
+  - `length`
+
+    Max leaderboard length. Default: `Infinity`
+
+##Methods
+
+  - `add(member, score, λ)`
+
+    Ranks a member in the leaderboard.
+
+        board.add('borbit', 100500, function(err, rank) {
+          // rank - current position, -1 if a member didn't
+          // fall within the leaderboard
+        });
+
+  - `rank(member, λ)`
+
+    Ranks a member in the leaderboard.
+
+        board.rank('borbit', function(err, rank) {
+          // rank - current position, -1 if a member didn't
+          // fall within the leaderboard
+        });
+
+##Tests
+
+Leaderboard is covered by [Mocha](http://visionmedia.github.com/mocha/). To run tests:
+
+     $ npm test
+
+## License 
+
+[MIT](http://en.wikipedia.org/wiki/MIT_License#License_terms). Copyright (c) 2012 Serge Borbit &lt;serge.borbit@gmail.com&gt;
--- test/leaderboard.js
+++ test/leaderboard.js
@@ -2,17 +2,74 @@ var redis = require('redis');
 var assert = require('assert');
 var Leaderboard = require('../');
 
-beforeEach(function(done) {
-  var client = redis.createClient();
-  client.on('connect', function() {
-    client.select(15, function() {
-      client.flushdb(done);
-    });
-  });
+before(function(done) {
+  this.client = redis.createClient();
+  this.client.select(15, done);
 });
 
 describe('Leaderboard', function() {
-  it('should be initialized with default options', function() {
-    assert.ok(true);
+  
+  describe('"add" method', function() {
+    // Initialize a subject leaderboard and 
+    // empty database before the suite
+    before(function(done) {
+      this.board = new Leaderboard('test');
+      this.client.flushdb(done);
+    });
+    
+    it('should return 0 for the member "member1" with score 30', function(done) {
+      this.board.add('member1', 30, function(err, rank) {
+        assert.equal(rank, 0);
+        done();
+      });
+    });
+
+  });
+
+  describe('"rank" method', function() {
+    // Initialize a subject leaderboard and 
+    // empty database before the suite
+    before(function(done) {
+      this.board = new Leaderboard('test');
+      this.client.flushdb(done);
+    });
+    
+    it('should return currect rank #1', function(done) {
+      var board = this.board;
+      board.add('member1', 50, function() {
+        board.rank('member1', function(err, rank) {
+          assert.equal(rank, 0);
+          done();
+        });
+      });
+    });
+
+    it('should return currect rank #2', function(done) {
+      var board = this.board;
+      board.add('member2', 40, function() {
+        board.rank('member2', function(err, rank) {
+          assert.equal(rank, 1);
+          done();
+        });
+      });
+    });
+
+    it('should return currect rank #3', function(done) {
+      var board = this.board;
+      board.add('member3', 30, function() {
+        board.rank('member3', function(err, rank) {
+          assert.equal(rank, 2);
+          done();
+        });
+      });
+    });
+
+    it('should return -1 if the user isn\'t in the leaderboard', function(done) {
+      this.board.rank('piska', function(err, rank) {
+        assert.equal(rank, -1);
+        done();
+      });
+    });
+
   });
 });
